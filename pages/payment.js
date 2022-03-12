7015de163c0b04e32a86bbe33031533622b80a7e
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-12 15:57:41 -0500

    in Layout: remove selected payment method on user logout, creates PaymentScreen to get payment method of user, in Store: creates new case to save payment method

---
+++ pages/payment.js
@@ -0,0 +1,112 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+import Form from '../components/Form';
+import CheckoutWizard from '../components/CheckoutWizard';
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  List,
+  ListItem,
+  Radio,
+  RadioGroup,
+  Typography,
+} from '@mui/material';
+import { useRouter } from 'next/router';
+import { Store } from '../utils/Store';
+import jsCookie from 'js-cookie';
+import { useSnackbar } from 'notistack';
+
+export default function PaymentScreen() {
+  const { enqueueSnackbar } = useSnackbar();
+  const router = useRouter();
+  const [paymentMethod, setPaymentMethod] = useState();
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress },
+  } = state;
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      router.push('/shipping');
+    } else {
+      setPaymentMethod(jsCookie.get('paymentMethod') || '');
+    }
+  }, [router, shippingAddress]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!paymentMethod) {
+      enqueueSnackbar('Payment method is required', { variant: 'error' });
+    } else {
+      dispatch({
+        type: 'SAVE_PAYMENT_METHOD',
+        payload: paymentMethod,
+      });
+      jsCookie.set('paymentMethod', paymentMethod);
+      router.push('/placeorder');
+    }
+  };
+
+  return (
+    <Layout title="Payment Method">
+      <CheckoutWizard activeStep={2}></CheckoutWizard>
+
+      <Form onSubmit={submitHandler}>
+        <Typography component="h1" variant="h1">
+          Payment Method
+        </Typography>
+
+        <List>
+          <ListItem>
+            <FormControl component="fieldset">
+              <RadioGroup
+                aria-label="Payment Method"
+                name="paymentMethod"
+                value={paymentMethod}
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              >
+                <FormControlLabel
+                  label="PayPal"
+                  value="PayPal"
+                  control={<Radio />}
+                ></FormControlLabel>
+
+                <FormControlLabel
+                  label="Stripe"
+                  value="Stripe"
+                  control={<Radio />}
+                ></FormControlLabel>
+
+                <FormControlLabel
+                  label="Cash"
+                  value="Cash"
+                  control={<Radio />}
+                ></FormControlLabel>
+              </RadioGroup>
+            </FormControl>
+          </ListItem>
+
+          <ListItem>
+            <Button fullWidth type="submit" variant="contained" color="primary">
+              Continue
+            </Button>
+          </ListItem>
+
+          <ListItem>
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              color="secondary"
+              onClick={() => router.push('/shipping')}
+            >
+              Back
+            </Button>
+          </ListItem>
+        </List>
+      </Form>
+    </Layout>
+  );
+}
--- utils/store.js
+++ utils/store.js
@@ -13,6 +13,9 @@ const initialState = {
     shippingAddress: Cookies.get('shippingAddress')
       ? JSON.parse(Cookies.get('shippingAddress'))
       : {},
+    paymentMethod: Cookies.get('paymentMethod')
+      ? JSON.parse(Cookies.get('paymentMethod'))
+      : '',
   },
   userInfo: Cookies.get('userInfo')
     ? JSON.parse(Cookies.get('userInfo'))
@@ -42,7 +45,7 @@ function reducer(state, action) {
 
       return { ...state, cart: { ...state.cart, cartItems } };
     }
-    case 'CART_REMOVE_ITEM':
+    case 'CART_REMOVE_ITEM': {
       const cartItems = state.cart.cartItems.filter(
         (item) => item._key !== action.payload._key
       );
@@ -50,6 +53,7 @@ function reducer(state, action) {
       Cookies.set('cartItems', JSON.stringify(cartItems));
 
       return { ...state, cart: { ...state.cart, cartItems } };
+    }
     case 'USER_LOGIN':
       return { ...state, userInfo: action.payload };
     case 'USER_LOGOUT':
@@ -67,6 +71,14 @@ function reducer(state, action) {
         cart: {
           ...state.cart,
           shippingAddress: action.payload,
+        },
+      };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     default:
